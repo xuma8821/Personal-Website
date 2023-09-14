50d0ce8909ec5885793dabb339508ee8e98f1b6d
xuehua ma <winniema@xuehuas-MacBook-Pro.local>
2023-09-14 16:29:32 +1000

    refact: refactor css to scss

--- script.js
+++ script.js
@@ -6,6 +6,33 @@ const msgInput = document.querySelector("#message");
 const submitBtn = document.querySelector(".contact-msg-btn");
 const contactForm = document.querySelector("form");
 const navToggleBtn = document.querySelector(".nav-toggle-btn");
+const allSections = document.querySelectorAll("section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section-hidden");
+  if (entry.target.classList.contains("skills-section")) {
+    const progressBars = document.querySelectorAll(".progress-bar");
+    progressBars.forEach(function (progressBar) {
+      const targetPercent = Number(progressBar.dataset.target);
+      let initialWidth = 0;
+      while (targetPercent > initialWidth) {
+        initialWidth++;
+        progressBar.style.width = `${initialWidth}%`;
+      }
+    });
+  }
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  section.classList.add("section-hidden");
+  sectionObserver.observe(section);
+});
+
 const slideToView = function (e) {
   e.preventDefault();
   if (e.target.classList.contains("nav-page-link")) {
